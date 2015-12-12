03606b90c4e509c72f399230217294a89c44e5f6
geospatialem <geospatialem@gmail.com>
2015-12-12 15:33:19 -0600

    Add try, except
    Add try, except for the user input file path.

--- scripts/setRelativePaths.py
+++ scripts/setRelativePaths.py
@@ -1,21 +1,44 @@
-#source: http://support.esri.com/fr/knowledgebase/techarticles/detail/40656
+# ---------------------------------------------------------------------------
+# Title: Set ArcMap .MXD Relative Paths (setRelativePaths.py)
+# Description: A python script that sets the relative path of an ArcMap
+# project(s), or .mxd files, within a user-defined file directory.
+# Author: Kitty Hurley
+# Original date: 2015-12-12
+# Last modified date: 2015-12-12
+# Resource: http://support.esri.com/fr/knowledgebase/techarticles/detail/40656
+# ---------------------------------------------------------------------------
 
-#Import ArcPy
-import arcpy, os
+# Import modules
+import arcpy, os, os.path
+from arcpy import env
 
-#Allow the user to set the workspace to search for the map documents
-Workspace = raw_input("Enter a directory location (e.g. G:\EH Tracking\GIS\GIS_Projects): ")
+# Allow the user to set the workspace to search for the map documents
+Workspace = raw_input('Enter a directory location (e.g. G:\EH Tracking\GIS\GIS_Projects): ')
 arcpy.env.workspace = Workspace
 
-#List the map documents in folder
-mxdList = arcpy.ListFiles("*.mxd")
+# Verify the user input is a file location using a try/except
+try:
+    if os.path.exists(Workspace):
+        print 'The following path was selected: ' + Workspace + '. The script will now set the relative path to the ArcMap projects in the directory.'
+# If the user input is incorrect, print a statement and quit the script.
+    else:
+        print 'An invalid directory was entered, please enter a valid directory to execute.'
+        print 'The script has unsuccessfully executed, and will close.'
+        quit()
+# Add an exception for the try/except
+except Exception as e:
+    print('An error has occurred.', e)
 
-#Set a relative path setting for each MXD in list.
+# List the map documents in folder
+mxdList = arcpy.ListFiles('*.mxd')
+
+# Set a relative path setting for each MXD in list.
 for file in mxdList:
-    #Set the map document to change the relative path
+    # Set the map document to change the relative path
     filePath = os.path.join(Workspace, file)
     mxd = arcpy.mapping.MapDocument(filePath)
-    #Set the relative paths property (to 'True')
+    # Set the relative paths property (to 'True')
     mxd.relativePaths = True
-    #Save the map doucment changes
+    # Save the map doucment changes
     mxd.save()
+    print 'The script has successfully executed.'
